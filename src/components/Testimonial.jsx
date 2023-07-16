3317d771eee0d7f5de88332ea9d30cbf7ee8b4f6
Utsab Kundu <kunutsab@gmail.com>
2023-07-17 03:05:42 +0530

    added infinite rolling cards

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -3,119 +3,93 @@ import TestimonialCard from "./TestimonialCard";
 
 /*To start the animation, go to Home.css .RightToLEft and remove the comment frm animation*/
 
+const test__content=[
+    {
+        text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.",
+        name:"Robert Brown",
+        post:"Software engineer",
+    },
+    {
+        text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.",
+        name:"Robert Brown",
+        post:"Software engineer",
+    },
+    {
+        text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.",
+        name:"Robert Brown",
+        post:"Software engineer",
+    },
+    {
+        text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.",
+        name:"Robert Brown",
+        post:"Software engineer",
+    },
+    {
+        text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.",
+        name:"Robert Brown",
+        post:"Software engineer",
+    },
+    {
+        text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.",
+        name:"Robert Brown",
+        post:"Software engineer",
+    },
+    {
+        text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.",
+        name:"Robert Brown",
+        post:"Software engineer",
+    },
+    {
+        text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.",
+        name:"Robert Brown",
+        post:"Software engineer",
+    },
+    {
+        text:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.",
+        name:"Robert Brown",
+        post:"Software engineer",
+    },
+]
+
 function Testimonial(){
+
+    const TC=test__content.map((data)=>{
+        return <TestimonialCard
+                    text={data.text}
+                    name={data.name}
+                    post={data.post}
+                />
+    });
     return(
         <>
             <div className="container testi-heading">
                 <p>Testimonials</p>
                 <h2>Proof of love by Clients</h2>
             </div>
-            <div className="testi_container">
-            <div className="scroll">
-                <div className="RightToLeft">
-                    
-                    {/* 9cards */}
-                    
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                </div>
-                <div className="LeftToRight">
-                    
-                    {/* 9cards */}
-                    
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                    <TestimonialCard
-                        text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, voluptas."
-                        name="Robert Brown"
-                        post="Software engineer"
-                    />
-                </div>
+            <div className="asasasas1">
+            
+                <div className="asasasas2">
+                    {/* 2. */}
+                <div className="asasasas3">
+                {/* 3 */}
+                {TC}
+                {TC}
             </div>
             </div>
+            </div>
+
+            <div className="asasasas1">
+            
+                <div className="asasasas2">
+                    {/* 2. */}
+                <div className="asasasas4">
+                {/* 3 */}
+                {TC}
+                {TC}
+            </div>
+            </div>
+            </div>           
+           
         </>
     );
 };
