fbf11caeb228367b4f14cb58073219e36e69eba8
Utsab Kundu <kunutsab@gmail.com>
2023-07-26 23:07:26 +0530

    made changes final part 2

--- src/components/OverlayImageBanner.jsx
+++ src/components/OverlayImageBanner.jsx
@@ -26,7 +26,9 @@ function OverlayImageBanner(props){
         this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
         var that = this;
-        var delta = 200 - Math.random() * 100;
+        // var delta = 200 - Math.random() * 100;
+        var delta = 100 - Math.random() * 50;
+
 
         if (this.isDeleting) { delta /= 2; }
 
